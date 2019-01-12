66250bd7b17003b9fa897f92ff68d7c9e3545501
Fabian Schlenz <mail@fabianonline.de>
2019-01-12 13:04:03 +0000

    More buttons are now supported, README is added.

---
+++ README.md
@@ -0,0 +1,98 @@
+# wii2osc
+
+Send OSC messages depending on events on a wiimote.
+
+## Dependencies
+
+* `libcwiid1` with python bindings
+* `pyOSC`
+
+## Installation
+
+This describes the installation on a fresh installation of raspbian on a
+Raspberry 3. Although this app is not fixed to be run on this platform, you
+will have to figure out how to install it elsewhere by yourself.
+
+* `sudo apt-get update`
+* `sudo apt-get install python-cwiid python-pip git`
+* `git clone https://github.com/fabianonline/wii2python.git`
+* `cd wii2osc`
+* `pip install -r requirements.txt`
+
+## Usage
+
+There are two ways of running python2wii: With fixed bluetooth addresses and
+aliases or dynamically. The first method requires a bit more work to
+configure, but has the nice advantage of being able to use a fixed alias per
+Wiimote.
+
+In every case you'll need the IP address and port of an OSC server. Many
+professional software will have this included; check the docs to learn if /
+how to enable it and which port is being used.
+
+### Dynamically
+
+Just run the tool with three parameters:
+* The IP address of the OSC server,
+* the port the OSC server is listening on and
+* the number of Wiimotes you want to use.
+
+So, for example if your OSC server is listening on port 8000 on IP address
+10.10.2.33 and you want to use 2 Wiimotes, you'd run:
+
+`python wii2osc.py 10.10.2.33 8000 2`
+
+The tool will then connect to any two Wiimotes, giving the first one the
+alias "1" and the other one the alias "2".
+
+### Fixed addresses and aliases
+
+You'll need the bluetooth addresses of each of your Wiimotes. Run `hcitool
+scan` and press buttons 1+2 on your Wiimote. It should appear in the scan
+results. Note it's address.
+
+Edit wii2osc.py. At the top there will be a block containing a bit of
+information followed by a line saying `addresses = {}`. There you can add
+the bluetooth address and an alias.
+
+Let's say the scan showed your Wiimote's address as being 00:1F:C5:50:AB:BA.
+You want to give this Wiimote the alias "fabian". Then modify the line to
+say:
+
+`addresses = {"00:1F:C5:50:AB:BA": "fabian"}`
+
+You can add multiple addresses and aliases:
+
+`addresses = {"00:1F:C5:50:AB:BA": "fabian", "00:1F:C5:54:7C:D0": "uli"}`
+
+Then run the tool with two parameters:
+* The IP address of the OSC server and
+* the port the OSC server is listening on.
+
+So, for example if your OSC server is listening on port 8000 on IP address
+10.10.2.33, you'd run:
+
+`python wii2osc.py 10.10.2.33 8000`
+
+The tool will then connect to the Wiimotes defined in the script.
+
+Note that even after modifying the file you can still use the dynamic method
+above by proving a third parameter. In that case, your given bluetooth
+addresses and aliases will be completely ignored.
+
+## OSC data
+
+The data sent via OSC to your OSC server will be:
+
+* To `wiimote/<alias>/main` whether buttons A or B on the Wiimote are
+  pressed.
+* To `wiimote/<alias>/nunchuk` whether buttons C or Z on a Nunchuk connected
+to the Wiimote are pressed.
+
+The data sent will be a float. By default, 1.0 is sent when a button is
+pressed and 0.0 is sent when the button is released.
+By pressing 1 or 2 on the Wiimote you can invert this values for the Wiimote
+or the Nunchuk, respectively. So, after pressing 2 on the Wiimote, a 1.0
+will be sent when no button is pressed on the Nunchuk and 0.0 is sent when a
+button is pressed. By pressing 2 again, you would revert to the default
+mapping.
--- wii2osc.py
+++ wii2osc.py
@@ -1,19 +1,38 @@
 #!/usr/bin/env python
 
 import cwiid
-import time, sys
+import time, sys, traceback
 import OSC
 
-if len(sys.argv)!=4 or sys.argv[1]=="--help" or sys.argv[1]=="-h":
-    print("Usage: {} <ip_of_osc_server> <port_of_osc_server> <number_of_wiimotes_to_use>".format(sys.argv[0]))
-    sys.exit
+############ Configuration - optional ############
+# You can define the bluetooth addresses of your wiimotes and give them aliases.
+# This gives you some advantages:
+# * The connection can be established a lot faster.
+# * You can set an alias for each wiimote which will be set everytime you use it.
+#
+# If you don't do this, you can just start this tool with a third parameter: The
+# number of Wiimotes you would like to use. In that case, that number of Wiimotes
+# is expected to connect to and the Wiimotes will just be numbered sequentially.
+# First Wiimote will send events to wiimote/1, second one to wiimote/2 and so on.
+#
+# If you want to use addresses with aliases, use the following syntax:
+# addresses = {"00:1F:C5:50:E9:B7": "fabian", "00:1F:C5:50:AB:BA": "uli"}
+# If you dont' want to use fixed addresses, use the following syntax:
+# addresses = {}
+
+addresses = {"00:1F:C5:50:E9:B7": "fabian", "00:19:1D:6D:9E:C6": "markus", "00:17:AB:3B:A5:78": "jan"}
+############ End of configuration ############
+
+if len(sys.argv)<3 or sys.argv[1]=="--help" or sys.argv[1]=="-h":
+    print("Usage: {} <ip_of_osc_server> <port_of_osc_server> (<number_of_wiimotes_to_use>)".format(sys.argv[0]))
+    sys.exit(1)
 
 wm = None
 wiimotes = []
 
 server_ip = sys.argv[1]
 server_port = int(sys.argv[2])
-wiimotes_count = int(sys.argv[3])
+wiimote_count = int(sys.argv[3]) if len(sys.argv)>=4 else None
 
 osc_client = OSC.OSCClient()
 print("Sending OSC messages to {}:{}.".format(server_ip, server_port))
@@ -22,79 +41,156 @@ osc_client.connect((server_ip, server_port))
 def log(str):
     print(str)
     
-def send_msg(address, value):
-    msg = OSC.OSCMessage()
-    msg.setAddress(address)
-    log("Sending: {} {}".format(address, value))
-    msg.append(value)
-    osc_client.send(msg)
+
 
 class MyWiimote:
-    def __init__(self, wm):
+    def __init__(self, wm, mac, alias):
         self.wm = wm
+        self.mac = mac
+        self.alias = alias
+        self.address = "wiimote/{}".format(alias)
+        self.wm.led = 1
+        self.wm.rpt_mode = cwiid.RPT_BTN | cwiid.RPT_NUNCHUK
+        #self.wm.enable(cwiid.FLAG_MESG_IFC)
+        #self.wm.mesg_callback = self.callback
+        #self.wm.request_status()
+        self.last_buttons = None
+        self.last_main = None
+        self.last_nunchuk = None
+        self.invert_main = False
+        self.invert_nunchuk = False
+        self.rumble_until = None
+        self.last_pressed = {}
+    
+    #def callback(self, data, ts):
+    #    print(data)
+    #
+    #def battery(self):
+    #    return self.wm.state["battery"] * 100.0 / cwiid.BATTERY_MAX
+    
+    def check_rumble(self):
+        if self.rumble_until is not None and self.rumble_until < time.time():
+            self.wm.rumble = False
+            self.rumble_until = None
+    
+    def check_invert(self, btn, btn_id, name):
+        if btn & btn_id:
+            self.log("Inverting {} buttons.".format(name))
+            self.rumble(0.5)
+            return True
+        else:
+            return False
+    
+    def rumble(self, length):
+        self.wm.rumble = True
+        self.rumble_until = time.time() + length
+        
+    def check_buttons(self):
+        state = self.wm.state
+        buttons = buttons_main = state['buttons']
+        
+        buttons_nunchuk = None
+        
+        if "nunchuk" in state:
+            buttons_nunchuk = state['nunchuk']['buttons']
+            stick = state['nunchuk']['stick']
+            if stick[0] < 64: buttons_nunchuk = buttons_nunchuk | cwiid.BTN_LEFT << 20
+            if stick[0] > 192:  buttons_nunchuk = buttons_nunchuk | cwiid.BTN_RIGHT << 20
+            if stick[1] < 64: buttons_nunchuk = buttons_nunchuk | cwiid.BTN_DOWN << 20
+            if stick[1] > 192:  buttons_nunchuk = buttons_nunchuk | cwiid.BTN_UP << 20
+            buttons = buttons | (buttons_nunchuk << 20)
+        
+        if buttons == self.last_buttons:
+            return
+        
+        self.invert_main ^= self.check_invert(buttons_main, cwiid.BTN_1, "main")
+        self.invert_nunchuk ^= self.check_invert(buttons_main, cwiid.BTN_2, "nunchuk")
+        
+        self.check_button(buttons_main, cwiid.BTN_A | cwiid.BTN_B, self.invert_main, "main", True)
+        self.check_button(buttons_main, cwiid.BTN_LEFT, False, "left", False)
+        self.check_button(buttons_main, cwiid.BTN_RIGHT, False, "right", False)
+        self.check_button(buttons_main, cwiid.BTN_UP, False, "up", False)
+        self.check_button(buttons_main, cwiid.BTN_DOWN, False, "down", False)
+        self.check_button(buttons_main, cwiid.BTN_MINUS, False, "minus", False)
+        self.check_button(buttons_main, cwiid.BTN_PLUS, False, "plus", False)
+        self.check_button(buttons_main, cwiid.BTN_HOME, False, "home", False)
+        
+        self.check_button(buttons_nunchuk, cwiid.NUNCHUK_BTN_C | cwiid.NUNCHUK_BTN_Z, self.invert_nunchuk, "nunchuk", True)
+        self.check_button(buttons_nunchuk, cwiid.BTN_LEFT << 20, False, "nunchuk_left", False)
+        self.check_button(buttons_nunchuk, cwiid.BTN_RIGHT << 20, False, "nunchuk_right", False)
+        self.check_button(buttons_nunchuk, cwiid.BTN_UP << 20, False, "nunchuk_up", False)
+        self.check_button(buttons_nunchuk, cwiid.BTN_DOWN << 20, False, "nunchuk_down", False)
+        
+        self.last_buttons = buttons
+    
+    def check_button(self, buttons, btnid, invert, name, long_pressable):
+        if buttons is None:
+            return
+        pressed = (buttons & btnid != 0) ^ invert
+        if long_pressable and (not btnid in self.last_pressed or pressed != self.last_pressed[btnid]):
+            value = 1.0 if pressed else 0.0
+            self.send_msg(name, value)
+            self.last_pressed[btnid] = pressed
+        elif not long_pressable:
+            if pressed and (not btnid in self.last_pressed or pressed != self.last_pressed[btnid]):
+                self.send_msg(name)
+            self.last_pressed[btnid] = pressed
+    
+    def send_msg(self, key, value=None):
+        addr = self.address + "/" + key
+        self.log("Sending: {} {}".format(addr, value))
+        
+        msg = OSC.OSCMessage()
+        msg.setAddress(addr)
+        if value is not None: msg.append(value)
+        osc_client.send(msg)
+        
+    def step(self):
+        self.check_rumble()
+        self.check_buttons()
+    
+    def log(self, message):
+        if True:
+            print("Wiimote \"{}\": {}".format(self.alias, message))
 
-print("Trying to connect to {} Wiimotes...".format(wiimotes_count))
-while len(wiimotes) < wiimotes_count:
-    print("Connecting to Wiimote #{}...".format(len(wiimotes)+1))
-    while not wm:
+def connect(mac, alias):
+    if mac is not None:
+        print("Connecting to Wiimote \"{}\" with address {}...".format(alias, mac))
+    else:
+        print("Connecting to unspecified Wiimote #{}...".format(alias))
+    wm = None
+    while wm is None:
         try:
-            wm = cwiid.Wiimote()
+            if mac is not None:
+                wm = cwiid.Wiimote(mac)
+            else:
+                wm = cwiid.Wiimote()
             print("Connected.")
-            wm.led = len(wiimotes)+1
-            wm.rpt_mode = cwiid.RPT_BTN | cwiid.RPT_NUNCHUK
-            wiimotes.append({"wiimote": wm, "last_buttons": None, "last_main": None, "last_nunchuk": None, "invert_main": False, "invert_nunchuk": False, "rumble_until": None})
+            wiimotes.append(MyWiimote(wm, mac, alias))
         except RuntimeError:
             pass
-    wm = None
-            
-while True:
-    for id, data in enumerate(wiimotes, start=1):
-        wm = data["wiimote"]
-        state = wm.state
-        btn = state['buttons']
-        
-        if data["rumble_until"] is not None and data["rumble_until"] < time.time():
-            wm.rumble = False
-            data["rumble_until"] = None
-        
-        btn_nc = state['nunchuk']['buttons'] if "nunchuk" in state else None
-        btn_nc_number = 0 if btn_nc is None else btn_nc
-        
-        buttons = btn | btn_nc_number
-        
-        if buttons == data['last_buttons']:
-            continue
-        #log(data)
-        
-        data['last_buttons'] = buttons
-        
-        address = "wiimote/{}".format(id)
-        
-        #log(state)
-        #log("Looking at Wiimote #{}".format(id))
-        
-        if (btn & cwiid.BTN_1):
-            data["invert_main"] = not data["invert_main"]
-            print("Main buttons of Wiimote #{} are now inverted: {}".format(id, data["invert_main"]))
-            data["rumble_until"] = time.time() + 0.5
-            wm.rumble = True
-        if (btn & cwiid.BTN_2 and btn_nc is not None):
-            data["invert_nunchuk"] = not data["invert_nunchuk"]
-            print("Nunchuk buttons of Wiimote #{} are now inverted: {}".format(id, data["invert_nunchuk"]))
-            data["rumble_until"] = time.time() + 0.5
-            wm.rumble = True
-        
-        main_btn_pressed = (btn & (cwiid.BTN_A | cwiid.BTN_B) != 0) ^ data["invert_main"]
-        if main_btn_pressed != data["last_main"]:
-            value = 1.0 if main_btn_pressed else 0.0
-            send_msg(address + "/main", value)
-            data["last_main"] = main_btn_pressed
-        
-        if btn_nc is not None:
-            nc_btn_pressed = (btn_nc & (cwiid.NUNCHUK_BTN_C | cwiid.NUNCHUK_BTN_Z) != 0) ^ data["invert_nunchuk"]
-            if nc_btn_pressed != data["last_nunchuk"]:
-                value = 1.0 if nc_btn_pressed else 0.0
-                send_msg(address + "/nunchuk", value)
-                data["last_nunchuk"] = nc_btn_pressed
-    time.sleep(0.05)
 
+if wiimote_count is not None:
+    print("Trying to connect to {} wiimotes...".format(wiimote_count))
+    for i in xrange(wiimote_count):
+        connect(None, str(i+1))
+elif len(addresses)>0:
+    print("Trying to connect to the {} wiimotes defined in addresses...".format(len(addresses)))
+    for mac, alias in addresses.items():
+        connect(mac, alias)
+else:
+    print("No addresses set and no wiimote_count given on the command line. I don't know what to do. Quitting.")
+    sys.exit(1)
+
+
+############ Main loop #################
+while True:
+    for wiimote in wiimotes:
+        try:
+            wiimote.step()
+        except:
+            print("An unexpected error occurred. I will just ignore it, but it should definitely not happen. Please report it on github.")
+            print("The error was:")
+            traceback.print_exc()
+            print()
+    time.sleep(0.05)
